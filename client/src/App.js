de0addb09cbffa6f1ea2a184143b61167bc0b471
Raymond Kiprop <kray435@gmail.com>
2025-09-16 17:43:57 +0300

    moving on to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React. {useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [entries, setEntries] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/entries")
+       .then(res => res.json())
+       .then(data => setEntries(data))
+       .catch(err => console.error("Error fetching entries:", err));
+  }, []);
+
+//add new entry
+  const addEntry = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/api/entries")
+      method: "Post", 
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
+    });
+    const newEntry = await res.json();
+    setEntries([...entries, newEntry]);
+    setTitle("");
+    setContent("");
+   )}
+
+   const deleteEntry = async (id) => {
+     await fetch('http://localhost:5000/api/entries/${id}`, {
+       method:"Delete",
+     });
+     setEntried(entries.filter((entry._id  !== id));
+   };
+
+
+   return (
+     <div style={{ padding: "20px" }}>
+       <h1>Lore Manager</h1>
+
+      <form onSubmit ={addEntry}>
+       <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholde="Title"
+          required
+       />
+       <input
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          required
+       />
+         <button type="submit">Add</button>
+        </form>
+
+       <ul>
+         {entries.map((entry) => (
+          <li key={entry._id}>
+             <strong>{entry.title}</strong>: {entry.content}
+             <button onClick={() => deleteEntry(entry._id)}>❌</button>
+           />
+          ))}
+        </ul>
+     </div>
+   );
+ }
 
 export default App;
